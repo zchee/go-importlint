2de4f9fa06f68f1265f7260a396196aa38bb5ded
Koichi Shiraishi <zchee.io@gmail.com>
2017-08-01 19:31:54 +0900

    doc: add package document
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2017 The go-importlint Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package importlint provide linter of package imports.
+package importlint
