f2a88af08e17daf1166556447ca56e4f6d26fd09
Koichi Shiraishi <zchee.io@gmail.com>
2017-08-01 18:45:28 +0900

    path: add IsNotExist function
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ path.go
@@ -0,0 +1,16 @@
+// Copyright 2017 The go-importlint Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package importlint
+
+import (
+	"os"
+)
+
+func isNotExist(path string) bool {
+	if _, err := os.Stat(path); err != nil && os.IsNotExist(err) {
+		return true
+	}
+	return false
+}
